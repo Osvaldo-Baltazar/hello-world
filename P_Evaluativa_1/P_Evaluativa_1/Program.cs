4a41473f5c97badc17eff1f6df7d74b851643ddc
Osvaldo Baltazar <55269095+Osvaldo-Baltazar@users.noreply.github.com>
2019-11-26 22:57:15 -0800

    Add files via upload

---
+++ P_Evaluativa_1/P_Evaluativa_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P_Evaluativa_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operacion ope = new Operacion();
+          
+           
+        }
+    }
+}
